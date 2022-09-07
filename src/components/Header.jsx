a019b978e53ebe0e939a577a3d3f45342985ae5e
AnkitSingh60 <ankitanwar730@gmail.com>
2022-09-07 23:55:01 +0530

    css added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Button } from '@mui/material';
+import { useNavigate } from "react-router-dom";
+
 
 const Header = () => {
+    let navigate = useNavigate();
+
     const ResetButton = () => {
-        window.location.reload();
+        navigate("/")
       };
     return (
         <div className="headerContainer">
             <div className="buttoncontainer">
-               <div className="btn1">
-               <Button style={{marginLeft:"100px" }} variant="contained" color="success"  className="reset" onClick={() => ResetButton()} >Home</Button>
-               </div>
+              
                 <div className="btn2">
-                <Button variant="contained" color="success" size="medium" className="reset" style={{marginLeft:"100px", marginTop:"5px" }}  onClick={() => ResetButton()}>RESET</Button>
+                <Button variant="contained" color="success" size="medium" className="reset" style={{marginLeft:"40px", marginTop:"5px" }}  onClick={() => ResetButton()}>HOME</Button>
                 </div>
             </div>
         </div>
